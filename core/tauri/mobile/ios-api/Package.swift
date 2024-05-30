838e884412983ad49a7f9aed0eabfb7dfd3a23a2
Eugene Hauptmann <eugene@reactivelions.com>
2024-05-29 21:38:36 -0400

    downgraded swift to 5.10

--- core/tauri/mobile/ios-api/Package.swift
+++ core/tauri/mobile/ios-api/Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:6.0
+// swift-tools-version:5.10
 // Copyright 2019-2024 Tauri Programme within The Commons Conservancy
 // SPDX-License-Identifier: Apache-2.0
 // SPDX-License-Identifier: MIT
